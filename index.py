44dcf424bd54f43aaa430c604ac4582d3c1eca17
Luiz Fernando Duarte <luizfernando.duartevidal1@gmail.com>
2024-03-28 09:16:49 -0300

    Projetinho black mirror

---
+++ index.py
@@ -0,0 +1,24 @@
+while True:
+    pergunta = input('Digite sua duvida sobre a serie black mirror, episodio JOAN IS AWFUL: ')
+         
+
+    if pergunta == "QUAL É O NOME COMPLETO DA PROTAGONISTA DO EPISÓDIO?":
+        print('O nome da protagonista do episodio é joan taif')
+    elif pergunta == 'QUEM DIRIGE A VIDA DE JOAN EM TEMPO REAL PARA UMA SÉRIE DE TELEVISÃO?':
+        print('Uma inteligencia artificial')
+    elif pergunta == 'QUAL É O NOME DO SERVIÇO DE STREAMING FICTÍCIO QUE PARODIA A NETFLIX NO EPISÓDIO?':
+        print('O nome é "Streamberry"')
+    elif pergunta == 'COMO JOAN DESCOBRE A EXISTÊNCIA DA SÉRIE SOBRE SUA VIDA?':
+        print('Quando joan decide assistir um filme com seu namorado')
+    elif pergunta == 'QUAL É A REAÇÃO INICIAL DE JOAN AO DESCOBRIR A SÉRIE E O QUE ELA FAZ EM RESPOSTA?':
+        print('joan fica incredula e pensa ser coisa de seu namorado, após a "ficha cair" Joan tenta desligar a TV para que outras coisas não fosse revelada')
+    elif pergunta == 'QUAIS SÃO OS TEMAS PRINCIPAIS EXPLORADOS NESTE EPISÓDIO DE BLACK MIRROR?':
+            print('Além de ter como pauta o uso da Inteligencia artifical e a rapidez com que nos tras respostas, a principal critica do episodio é que')
+    elif pergunta == 'O EPISÓDIO TERMINA DE MANEIRA TÍPICA PARA A SÉRIE BLACKMIRROR? EXPLIQUE.':
+        print('Sim, por conta do plost twist, no episodio, assim como nos outros, a tramatermia de forma que o telespectador n imagina, gerando o sentimento de extase e confusâo')
+    
+
+
+
+    
+
